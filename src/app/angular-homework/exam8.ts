15028ce96b5328cf2bd2e2435fb9dc0f643b1f78
TuHT <tuht@arrowhitech.co>
2024-01-02 15:24:08 +0700

    workhome

--- src/app/angular-homework/exam8.ts
+++ src/app/angular-homework/exam8.ts
@@ -1,4 +1,4 @@
-import { factorial, isPrime } from "./exam8";
+import { factorial, isPrime } from "./shared/exam8";
 
 console.log('Factorial: ' + factorial(2))
 console.log('isPrime: ' + isPrime(2))
